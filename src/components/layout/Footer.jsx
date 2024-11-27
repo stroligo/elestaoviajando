f62eabf1b33ba805d9cd1091fe192be82b89a000
stroligo <gabrielstroligo@gmail.com>
2024-11-27 01:56:40 +0100

    front dev

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,17 +4,24 @@ import Logo from '/assets/img/logo.png';
 import { Button } from '../ui/Button';
 export function Footer() {
   return (
-    <footer className="bg-gradient-to-t from-orange to-yellow">
+    <footer className=" bg-orange">
       <div className="container mx-auto py-10 text-white">
-        <div className="flex justify-between">
-          <figure className="w-40">
-            <img src={Logo} alt="Logo" />
-          </figure>
-          <div className="flex flex-row gap-8">
-            <div>
-              <h6>Redes Sociais</h6>
+        <div className="flex justify-between flex-col md:flex-row h-full">
+          <div className="p-4 h-full items-center justify-center ">
+            <figure className=" bg-orange">
+              <img src={Logo} alt="Logo" className="max-w-40" />
+            </figure>
+          </div>
+          <div className="flex flex-row gap-8 md:w-4/12 bg-blue-dark text-white p-5 rounded-xl pa">
+            <div className="">
+              <div className="text-xl font-semibold mb-2">Redes Sociais</div>
+              <div>
+                Ter um sonho e ser capaz de finalmente torná-lo realidade é a
+                melhor sensação de todos os tempos. E, para nós, isso faz mais
+                sentido ainda ao podermos compartilhar com você.
+              </div>
             </div>
-            <ul className="flex gap-2 flex-col">
+            <ul className="w-fit flex-1 flex gap-2 flex-col">
               <Button>
                 <Link href="/">Home</Link>
               </Button>
