16a2d25bc9e8fcb738e944d43e94fbc4c130ac9a
stroligo <gabrielstroligo@gmail.com>
2024-12-06 17:17:47 -0300

    usando tailwind normal

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ be16c876a4dd3f05bb88eeeef8d8e7fd
 ajusta a KEY do tempo para .env
 
 fazer modo noturno
+
+ajustar degrade radial claro para o bg
+
+fazer o formulario de envio
--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,9 @@ import { Footer } from './components/layout/Footer';
 import { Main } from './components/layout/Main';
 import { Routes } from './routes/Routes';
 
-/**
- * App component serves as the root component of the application.
- *
- * This component sets up the main layout of the application, including
- * the header, main content area, and footer. It utilizes a flexbox layout
- * to ensure the application occupies the full height of the screen.
- *
- * Components:
- * - Header: Displays the top navigation or branding.
- * - Main: Contains the main content area, where the Routes component manages
- *   the rendering of different pages based on the current route.
- * - Footer: Displays the bottom section of the application.
- *
- * The component ensures that the entire application is rendered with
- * minimal visual artifacts through the use of antialiasing.
- */
 export function App() {
   return (
-    <div className="flex flex-col min-h-screen antialiased">
+    <div className="flex flex-col min-h-screen antialiased ">
       <Header />
       <Main>
         <Routes />
